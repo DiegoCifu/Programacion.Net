f661096a7f844c324bd6d43110458b22223f0d86
DIEGO Cifuentes <diegocifuentesposada@gmail.com>
2024-09-26 15:36:45 +0200

    Intro código

---
+++ While/While/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Intentos averiguar la contraseña
+string contraseña = "1234";
+string contraseñaIntroducida = "";
+
+int intentos = 0;
+
+while (contraseñaIntroducida != contraseña)
+{
+    Console.WriteLine("Introduce la contraseña:");
+    contraseñaIntroducida = Console.ReadLine();
+    intentos++;
+    if (intentos >= 3)
+    {
+        Console.WriteLine("¡Has superado el número de intentos!");
+        break;
+    }
+}
+Console.WriteLine("¡Contraseña correcta!");
