3972820d3e3369738aa644abefecce8b69fd181c
DIEGO Cifuentes <diegocifuentesposada@gmail.com>
2024-10-01 10:15:36 +0200

    Actualizacion

---
+++ SolucionClases/EjemploClases/Coche.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploClases
+{
+    public class Coche
+    {
+        string Marca;
+        string Modelo;
+        string Color;
+        double Consumo;
+        int Cilindrada;
+        int NumeroPuertas;
+        int NumeroRuedas;
+        int NumeroAsientos;
+    }
+}
