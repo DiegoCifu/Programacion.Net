92266feb1dc3a0e334bf1e696b93e03263746c58
DIEGO Cifuentes <diegocifuentesposada@gmail.com>
2024-10-07 10:58:37 +0200

    Código calculadora

---
+++ SolucionClases/EjemploCalculadora/Calculadora.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploCalculadora
+{
+    public class Calculadora
+    {
+        //Metodo que suma dos numeros
+
+        public double Sumar(double num1, double num2)
+        //Sumar sería el método
+        {
+            MostrarMensaje($"Se van a sumar dos números {num1} y {num2}");
+            return (num1 + num2);
+        }
+        public double Restar(double num1, double num2)
+
+        {
+            MostrarMensaje($"Se van a restar dos números {num1} y {num2}");
+            return (num1 - num2);
+        }
+
+        public double Multiplicar(double num1, double num2)
+
+        {
+            MostrarMensaje($"Se van a multiplicar dos números {num1} y {num2}");
+            return (num1 * num2);
+        }
+
+        public double Dividir(double num1, double num2)
+
+        {
+            if (num2 == 0)
+            {
+                MostrarMensaje("Error: No se puede dividir por cero.");
+                return 0;
+            }
+            MostrarMensaje($"Se van a dividir dos números: {num1} y {num2}");
+            return num1 / num2;
+        }
+
+        public void MostrarMensaje(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+       
+    }
+}
---
+++ SolucionClases/EjemploCalculadora/Program.cs
@@ -0,0 +1,64 @@
+﻿using EjemploCalculadora;
+
+// Crear una instancia de la calculadora
+
+Calculadora calculadora = new Calculadora();
+
+
+
+// implementar un menu para la calculadora
+bool salir = false;
+while (!salir)
+{
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Salir");
+    Console.WriteLine("Seleccine una opción");
+    string opcion = Console.ReadLine();
+
+    double num1, num2;
+
+    switch (opcion)
+    {
+        case "1":
+            Console.WriteLine("Ingrese el primer número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            double suma = calculadora.Sumar(num1, num2);
+            calculadora.MostrarMensaje($"El resultado de la suma es: {suma}");
+            break;
+        case "2":
+            Console.WriteLine("Ingrese el primer número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            double resta = calculadora.Restar(num1, num2);
+            calculadora.MostrarMensaje($"El resultado de la resta es: {resta}");
+            break;
+        case "3":
+            Console.WriteLine("Ingrese el primer número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            double multiplicacion = calculadora.Multiplicar(num1, num2);
+            calculadora.MostrarMensaje($"El resultado de la multiplicacion es: {multiplicacion}");
+            break;
+        case "4":
+            Console.WriteLine("Ingrese el primer número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            double division = calculadora.Dividir(num1, num2);
+            calculadora.MostrarMensaje($"El resultado de la división es: {division}");
+            break;
+        case "5":
+            salir = true;
+            break;
+        default:
+            calculadora.MostrarMensaje("Opcion inválida");
+            break;
+    }
+}
